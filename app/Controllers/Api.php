cc033db18f9c8a840e0e1855a95c8d49a63b46e0
agaring <arnaud.garing67@gmail.com>
2022-01-19 15:43:18 +0100

    Routage, premiere modification pour le controlleur de l'api et de la vue

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -4,11 +4,21 @@ namespace App\Controllers;
 
 use App\Models\ApiModel;
 
+/**
+ * Api controller
+ */
 class Api extends BaseController
 {
-
-    public function index()
+    /**
+     * Return a table of all API in project
+     */
+    public function index(): string
     {
-        echo view('api_view');
+        $arrApi = (new ApiModel())->findAll();
+
+        return view('api\api_list', [
+            'title' => 'Liste des Apis',
+            'arrApi' => $arrApi
+        ]);
     }
 }
